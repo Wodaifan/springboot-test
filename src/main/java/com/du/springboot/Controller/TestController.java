15285502627267b3ef9024cc2d292a9422d7a575
Wodaifan <74897044+Wodaifan@users.noreply.github.com>
2022-12-15 19:59:15 +0800

    Update TestController.java

--- src/main/java/com/du/springboot/Controller/TestController.java
+++ src/main/java/com/du/springboot/Controller/TestController.java
@@ -18,5 +18,6 @@ public class TestController {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }

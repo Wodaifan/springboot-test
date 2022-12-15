d7e4875db94c679769c130f2f27c5b5368961738
wodaifan <wodaifan@163.com>
2022-12-15 18:10:36 +0800

    second Commit

---
+++ src/main/java/com/du/springboot/Controller/TestController.java
@@ -0,0 +1,15 @@
+package com.du.springboot.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController("/test")
+public class TestController {
+
+    @GetMapping()
+    public void test(){
+        System.out.println("hello");
+    }
+}
